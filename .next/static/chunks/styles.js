7f9a3e8c9ce80602e4a2e5749b023389fa013add
macandrew6 <mr.betterandrewmac@gmail.com>
2019-10-22 14:33:36 -0700

    adds a server to project

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1571441972176");
+            injectCss(link, link.href.split("?")[0] + "?unix=1571442679029");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1571441972171");
+            injectCss(link, link.href.split("?")[0] + "?unix=1571442679024");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "188a7c6e9cd62f9f7aaf";
+/******/ 	var hotCurrentHash = "ecec6b917818048b2127";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
